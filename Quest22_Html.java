ac747b780437229b790c485925752ef6fcb3c9f6
sumeyse <80817179+sumeyse@users.noreply.github.com>
2021-04-29 15:49:34 +0300

    Update Quest22_Html.java

--- Quest22_Html.java
+++ Quest22_Html.java
@@ -4,13 +4,17 @@ public class Quest22_Html {
 
 	public static void main(String[] args) {
 		
-		String html="<!DOCTYPE html><html><head><title>Java</title></head><body><p id=\"myid\"></p></body></html>";
+		
+                String html="<!DOCTYPE html><html><head><title>Java</title></head><body><p id=\"myid\"></p></body></html>";
 
+		if(!html.contains("html")) {
+			System.out.println("invalid input!");
+		}else {
 		int start= html.indexOf('"');
 		int fin= html.indexOf('"', start+1);
 
 		String output=html.substring(start+1,fin);
 		System.out.println(output);
-	}
+	}}
 
 }
